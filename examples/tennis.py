61fca3f522eae4a22d7e6974281df3586ea8f0a7
Daniel Garnier-Moiroux <git@garnier.wf>
2022-02-17 17:47:23 +0100

    rework tennis example

--- examples/tennis.py
+++ examples/tennis.py
@@ -1,33 +1,41 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    score: int = 0
+
 class Game:
+    p1 = 1
+    p2 = 2
 
     __scores = ["0", "15", "30", "40", "Ad"]
 
     def __init__(self):
-        self.p1_score = 0
-        self.p2_score = 0
+        self.p1 = Player()
+        self.p2 = Player()
 
-    def p1_score_point(self):
-        if self.p1_score > 3 and self.p2_score - self.p1_score == 1:
-            self.p2_score -= 1
-        if self.p1_score == 3 and not self.__is_40A():
-            self.p1_score += 2
-        else:
-            self.p1_score += 1
+    def score_point(self, player):
+        if player == Game.p1:
+            player = self.p1
+            other = self.p2
+        elif player == Game.p2:
+            player = self.p2
+            other = self.p1
 
-    def p2_score_point(self):
-        if self.p1_score > 3 and self.p1_score - self.p2_score == 1:
-            self.p1_score -= 1
-        elif self.p2_score == 3 and not self.__is_40A():
-            self.p2_score += 2
+        if other.score > 3 and other.score - player.score == 1:
+            other.score -= 1
+        elif player.score == 3 and not self.__is_40A():
+            player.score += 2
         else:
-            self.p2_score += 1
+            player.score += 1
 
     def __is_40A(self):
-        return self.p1_score == self.p2_score == 3
+        return self.p1.score == self.p2.score == 3
 
     def score(self):
-        if self.p2_score >= len(self.__scores):
+        if self.p2.score >= len(self.__scores):
             return "WIN P2"
-        elif self.p1_score >= len(self.__scores):
+        elif self.p1.score >= len(self.__scores):
             return "WIN P1"
-        return f"{self.__scores[self.p1_score]}:{self.__scores[self.p2_score]}"
+        return f"{self.__scores[self.p1.score]}:{self.__scores[self.p2.score]}"
--- test_8_tennis.py
+++ test_8_tennis.py
@@ -10,36 +10,36 @@ def test_no_points():
 def test_score_p1():
     game = Game()
 
-    game.p1_score_point()
+    game.score_point(Game.p1)
     assert game.score() == "15:0"
 
-    game.p1_score_point()
+    game.score_point(Game.p1)
     assert game.score() == "30:0"
 
-    game.p1_score_point()
+    game.score_point(Game.p1)
     assert game.score() == "40:0"
 
 
 def test_score_p2():
     game = Game()
 
-    game.p2_score_point()
+    game.score_point(Game.p2)
     assert game.score() == "0:15"
 
-    game.p2_score_point()
+    game.score_point(Game.p2)
     assert game.score() == "0:30"
 
-    game.p2_score_point()
+    game.score_point(Game.p2)
     assert game.score() == "0:40"
 
 
 def test_win_p2():
     game = Game()
 
-    game.p2_score_point()
-    game.p2_score_point()
-    game.p2_score_point()
-    game.p2_score_point()
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
 
     assert game.score() == "WIN P2"
 
@@ -47,10 +47,10 @@ def test_win_p2():
 def test_win_p1():
     game = Game()
 
-    game.p1_score_point()
-    game.p1_score_point()
-    game.p1_score_point()
-    game.p1_score_point()
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
 
     assert game.score() == "WIN P1"
 
@@ -58,14 +58,14 @@ def test_win_p1():
 def test_avantage():
     game = Game()
 
-    game.p1_score_point()
-    game.p1_score_point()
-    game.p1_score_point()
-    game.p2_score_point()
-    game.p2_score_point()
-    game.p2_score_point()
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
 
-    game.p1_score_point()
+    game.score_point(Game.p1)
 
     assert game.score() == "Ad:40"
 
@@ -73,14 +73,14 @@ def test_avantage():
 def test_avantage_go_back():
     game = Game()
 
-    game.p1_score_point()
-    game.p1_score_point()
-    game.p1_score_point()
-    game.p2_score_point()
-    game.p2_score_point()
-    game.p2_score_point()
-    game.p1_score_point()  # Ad:40
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
+    game.score_point(Game.p1)
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
+    game.score_point(Game.p2)
+    game.score_point(Game.p1)  # Ad:40
 
-    game.p2_score_point()
+    game.score_point(Game.p2)
 
     assert game.score() == "40:40"
