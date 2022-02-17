eabe07a343dc6811f60e5b98a1e74c50baabfe48
Daniel Garnier-Moiroux <git@garnier.wf>
2022-02-17 21:16:23 +0100

    tdd: tennis

--- examples/tennis.py
+++ examples/tennis.py
@@ -9,33 +9,17 @@ class Game:
     p1 = 1
     p2 = 2
 
-    __scores = ["0", "15", "30", "40", "Ad"]
+    __scores = ["0", "15", "30", "40"]
 
     def __init__(self):
-        self.p1 = Player()
-        self.p2 = Player()
+        self.score_1 = 0
+        self.score_2 = 0
 
     def score_point(self, player):
         if player == Game.p1:
-            player = self.p1
-            other = self.p2
+            self.score_1 += 1
         elif player == Game.p2:
-            player = self.p2
-            other = self.p1
-
-        if other.score > 3 and other.score - player.score == 1:
-            other.score -= 1
-        elif player.score == 3 and not self.__is_40A():
-            player.score += 2
-        else:
-            player.score += 1
-
-    def __is_40A(self):
-        return self.p1.score == self.p2.score == 3
+            self.score_2 += 1
 
     def score(self):
-        if self.p2.score >= len(self.__scores):
-            return "WIN P2"
-        elif self.p1.score >= len(self.__scores):
-            return "WIN P1"
-        return f"{self.__scores[self.p1.score]}:{self.__scores[self.p2.score]}"
+        return f"{self.__scores[self.score_1]}:{self.__scores[self.score_2]}"
--- test_8_tennis.py
+++ test_8_tennis.py
@@ -33,54 +33,23 @@ def test_score_p2():
     assert game.score() == "0:40"
 
 
-def test_win_p2():
-    game = Game()
-
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-
-    assert game.score() == "WIN P2"
-
-
-def test_win_p1():
+def test_scores():
     game = Game()
 
     game.score_point(Game.p1)
-    game.score_point(Game.p1)
-    game.score_point(Game.p1)
-    game.score_point(Game.p1)
-
-    assert game.score() == "WIN P1"
-
-
-def test_avantage():
-    game = Game()
-
-    game.score_point(Game.p1)
-    game.score_point(Game.p1)
-    game.score_point(Game.p1)
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-
-    game.score_point(Game.p1)
-
-    assert game.score() == "Ad:40"
-
-
-def test_avantage_go_back():
-    game = Game()
-
-    game.score_point(Game.p1)
-    game.score_point(Game.p1)
-    game.score_point(Game.p1)
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-    game.score_point(Game.p2)
-    game.score_point(Game.p1)  # Ad:40
+    assert game.score() == "15:0"
 
     game.score_point(Game.p2)
+    assert game.score() == "15:15"
 
+    game.score_point(Game.p2)
+    assert game.score() == "15:30"
+
+    game.score_point(Game.p1)
+    assert game.score() == "30:30"
+
+    game.score_point(Game.p1)
+    assert game.score() == "40:30"
+
+    game.score_point(Game.p2)
     assert game.score() == "40:40"
